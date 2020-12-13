f8e8e9515e74e2f08acab6c8e9a6574d84c3cdf5
angelo milien <angelomilienjoseph@gmail.com>
2020-12-12 22:18:02 -0500

    done

--- bin/sql_runner.rb
+++ bin/sql_runner.rb
@@ -18,6 +18,6 @@ class SQLRunner
   end
 
   def execute_sql(sql)
-     sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
+    sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
   end
 end
